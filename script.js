ae5a2aa10af2e6552d17b60ff0d286d52036165b
emmanuel gonzalez <emmanuel.gonzalez903@gmail.com>
2024-01-12 12:44:34 -0300

    segundo commit

---
+++ script.js
@@ -0,0 +1,16 @@
+// print("------------FACTORIAL DE NUMEROS")
+// num3 = int(input("Ingrese un numero: "))
+
+// for n in range(num3-1, 1, -1):
+//     num3 = num3 * n
+// print(num3)
+// print("")
+
+// // transformar a js 
+
+console.log("Factorial de numeros")
+Number3 = parseInt(prompt("ingrese un numero"))
+for (let n = Number3 -1; n >= 1 ; --n){
+    Number3 = Number3 * n
+    }
+console.log(Number3)
